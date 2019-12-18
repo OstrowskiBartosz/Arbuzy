aa2b79099376876b3faa0e95b7129cd9a6b467e2
Bartosz_Ostrowski <bartosz.ostrowski123@gmail.com>
2019-12-18 21:00:42 +0100

    localhost

--- api/routes/database_connection.js
+++ api/routes/database_connection.js
@@ -5,8 +5,8 @@ var mysql = require('mysql');
 var session = require('express-session');
 
 var con = mysql.createConnection({
-  host: '10.128.9.155',
-  user: 'arbuz',
+  host: 'localhost',
+  user: 'root',
   password: 'lolo',
   database: "mydb",
   charset : 'utf8_unicode_ci',
