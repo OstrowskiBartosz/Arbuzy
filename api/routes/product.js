a86b658c54ee77ae6c634b4807920f3f72f9fe79
Robinxon <robinxon.pl@gmail.com>
2020-01-20 23:42:34 +0100

    wczytywanie danych produktu

--- api/routes/product.js
+++ api/routes/product.js
@@ -4,11 +4,47 @@ var con = require("./database_connection");
 router.use(express.json());
 
 router.post("/", function(req, res, next) {
-  let id_produktu = req.body.productID;
-  var sql = "SELECT * FROM produkty WHERE id_produktu = '" + id_produktu + "';";
+  var productData = { error: true };
+  let productID = req.body.productID;
+  var sql =
+    "SELECT p.id_produktu, p.id_kategorii, ka.nazwa_kategorii, p.id_producenta, pr.nazwa_producenta, p.nazwa_produktu, p.opis_produktu " +
+    "FROM ((produkty p " +
+    "INNER JOIN kategorie ka ON p.id_kategorii = ka.id_kategorii) " +
+    "INNER JOIN producenci pr ON p.id_producenta = pr.id_producenta) " +
+    "WHERE id_produktu = " +
+    productID +
+    ";";
   con.query(sql, function(err, result) {
-    res.send(JSON.stringify(result));
-    res.end();
+    if (result.length > 0) {
+      productData = {
+        ...productData,
+        error: false,
+        productID: req.body.productID,
+        produktIDKategorii: result[0].id_kategorii,
+        productNazwaKategorii: result[0].nazwa_kategorii,
+        produktIDProducenta: result[0].id_producenta,
+        productNazwaProducenta: result[0].nazwa_producenta,
+        productNazwa: result[0].nazwa_produktu,
+        productOpis: result[0].opis_produktu
+      };
+      var sql =
+        "SELECT atrybut, wartosc, typ " +
+        "FROM atrybuty " +
+        "WHERE id_produktu = " +
+        productID +
+        ";";
+      con.query(sql, function(err, result) {
+        productData = {
+          ...productData,
+          atrybuty: result
+        };
+        res.send(JSON.stringify(productData));
+        res.end();
+      });
+    } else {
+      res.send(JSON.stringify(productData));
+      res.end();
+    }
   });
 });
 
